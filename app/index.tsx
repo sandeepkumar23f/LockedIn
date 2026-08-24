28f00794a898f1363d4ca93376dd69e11339a47c
sandeepkumar23f <sandeep21000552@gmail.com>
2026-08-24 08:41:15 +0530

    project setup with nativewind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-green-500">
+      <Text className="text-white text-4xl">Hello NativeWind!</Text>
+    </View>
+  );
+}
